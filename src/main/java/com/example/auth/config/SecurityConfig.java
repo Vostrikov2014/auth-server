8bdf447456046bbf10f14768894955a22fe9ec66
45VostrikovDA <45vostrikovda@region.cbr.ru>
2025-04-08 17:30:41 +0300

    2

--- src/main/java/com/example/auth/config/SecurityConfig.java
+++ src/main/java/com/example/auth/config/SecurityConfig.java
@@ -3,12 +3,20 @@ package com.example.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -21,12 +29,30 @@ public class SecurityConfig {
         OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
                 OAuth2AuthorizationServerConfigurer.authorizationServer();
 
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-                .oidc(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
 
-        http.exceptionHandling((e) ->
-                e.authenticationEntryPoint(
-                        new LoginUrlAuthenticationEntryPoint("/login"))
+        http.cors(cors ->
+                cors.configurationSource(corsConfigurationSource())
+        );
+
+        http.securityMatcher(authorizationServerConfigurer.getEndpointsMatcher());
+
+        http.with(authorizationServerConfigurer, (authorizationServer) ->
+                authorizationServer
+                        .oidc(Customizer.withDefaults()));    // Enable OpenID Connect 1.0
+
+        http.authorizeHttpRequests(authorize ->
+                authorize.requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
+        );
+
+        // Redirect to the login page when not authenticated from the
+        // authorization endpoint
+        http.exceptionHandling((exceptions) -> exceptions
+                .defaultAuthenticationEntryPointFor(
+                        new LoginUrlAuthenticationEntryPoint("/login"),
+                        new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                )
         );
 
         return http.build();
@@ -38,9 +64,24 @@ public class SecurityConfig {
 
         http.formLogin(Customizer.withDefaults());
         http.authorizeHttpRequests(
-                c -> c.anyRequest().authenticated()
+                c -> c.requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
         );
 
         return http.build();
+    }
+
+    // Настройка CORS
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:9090"));      // Разрешить запросы с порта 3000
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));  // Разрешить методы
+        configuration.setAllowedHeaders(List.of("*"));                          // Разрешить любые заголовки
+        configuration.setAllowCredentials(true);                                   // Разрешить отправку cookie
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);             // Применение ко всем маршрутам
+        return source;
     }
 }
